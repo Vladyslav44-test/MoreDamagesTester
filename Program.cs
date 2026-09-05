279f635aacd913720316803e096973b4db349a5e
Vladislav44-test <elertvelert@gmail.com>
2026-09-05 11:33:53 +0300

    Трохи підправив повідомлення, щоб вони виглядали нормально

--- Program.cs
+++ Program.cs
@@ -16,10 +16,10 @@ namespace MoreDamagesTester
             ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
             while (true)
             {
-                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit");
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
                 char key = Console.ReadKey().KeyChar;
                 if ((key != '0') && (key != '1') && (key != '2') && (key != '3')) return;
-                Console.Write("\nS for sword, A for arrow, anything else to quit");
+                Console.Write("\nS for sword, A for arrow, anything else to quit: ");
                 char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
                 switch (weaponKey)
                 {
